b875bae6dc2f879f08ab2bf97ee2a91c24c926a5
bamooxa <47267318+bamooxa@users.noreply.github.com>
2021-09-19 08:28:38 +1000

    Update backtest.py
    removed btc compare

--- backtest.py
+++ backtest.py
@@ -5,10 +5,10 @@ from binance_trade_bot import backtest
 if __name__ == "__main__":
     history = []
     for manager in backtest(datetime(2021, 1, 1), datetime.now()):
-        btc_value = manager.collate_coins("BTC")
+        btc_value = 0
         bridge_value = manager.collate_coins(manager.config.BRIDGE.symbol)
         history.append((btc_value, bridge_value))
-        btc_diff = round((btc_value - history[0][0]) / history[0][0] * 100, 3)
+        btc_diff = 0
         bridge_diff = round((bridge_value - history[0][1]) / history[0][1] * 100, 3)
         print("------")
         print("TIME:", manager.datetime)
